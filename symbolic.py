97e5750876c6e82c3fa27248fc46b6f18d3dbda3
Zhengyao Lin <zhengyao-lin@outlook.com>
2023-04-05 21:37:32 -0400

    Fix issues in copying configuration

--- symbolic.py
+++ symbolic.py
@@ -3,47 +3,27 @@ from __future__ import annotations
 from typing import Tuple, Optional, List, Generator, Type, Dict, Mapping, Any, Set, Callable
 from dataclasses import dataclass, field
 
+import inspect
+
 import smt
 import permission
 
 from dataflow import DataflowGraph, ProcessingElement, ConstantValue, FunctionArgument
 
 
-class OperatorAction: ...
+TransitionFunction = Callable[..., Any]
 
 
 @dataclass
-class PopChannelAction(OperatorAction):
-    input_index: int
+class ChannelId:
+    id: int
 
 
 @dataclass
-class PushChannelAction(OperatorAction):
-    output_index: int
-    value: smt.SMTTerm
-
-
-@dataclass
-class ReadMemoryAction(OperatorAction):
-    base: smt.SMTTerm
-    index: smt.SMTTerm
-
-
-@dataclass
-class WriteMemoryAction(OperatorAction):
-    base: smt.SMTTerm
-    index: smt.SMTTerm
-    value: smt.SMTTerm
-
-
-@dataclass
-class BranchAction(OperatorAction):
+class Branching:
     condition: smt.SMTTerm
-    true_branch: Callable[[Operator], TransitionGenerator]
-    false_branch: Callable[[Operator], TransitionGenerator]
-
-
-TransitionGenerator = Generator[OperatorAction, Any, None]
+    true_branch: TransitionFunction
+    false_branch: TransitionFunction
 
 
 class Operator:
@@ -56,58 +36,81 @@ class Operator:
             return cls
         return wrapper
     
-    def __init__(self, pe: ProcessingElement):
+    def __init__(self, pe: ProcessingElement, transition: Optional[TransitionFunction] = None):
         self.pe = pe
+        self.current_transition: TransitionFunction = transition or type(self).start
 
-    def transition(self) -> TransitionGenerator:
+    def transition_to(self, transition: TransitionFunction):
+        self.current_transition = transition
+
+    def start(self, config: SymbolicConfiguration) -> TransitionGenerator:
         raise NotImplementedError()
     
     def copy(self) -> Operator:
-        return type(self)(self.pe)
+        return type(self)(self.pe, self.current_transition)
 
 
 @Operator.implement("ARITH_CFG_OP_ADD")
 class AddOperator(Operator):
-    def transition(self) -> TransitionGenerator:
-        a = yield PopChannelAction(0)
-        b = yield PopChannelAction(1)
-        yield PushChannelAction(0, smt.Plus(a, b))
+    def start(self, config: SymbolicConfiguration, a: ChannelId(0), b: ChannelId(1)) -> Tuple[ChannelId(0)]:
+        return smt.Plus(a, b)
 
 
 @Operator.implement("STREAM_FU_CFG_T")
 class StreamOperator(Operator):
-    def __init__(self, pe: ProcessingElement):
-        super().__init__(pe)
+    def __init__(self, *args):
+        super().__init__(*args)
 
         self.current: Optional[smt.SMTTerm] = None
         self.end: Optional[smt.SMTTerm] = None
 
-    def transition(self) -> TransitionGenerator:
-        if self.current is None:
-            first = yield PopChannelAction(0)
-            end = yield PopChannelAction(1)
-            step = yield PopChannelAction(2)
+    def start(self, config: SymbolicConfiguration, first: ChannelId(0), end: ChannelId(1)):
+        self.current = first
+        self.end = end
+        self.transition_to(StreamOperator.loop)
+        return ()
 
-            self.current = smt.Plus(first, step)
-            self.end = end
+    def loop(self, config: SymbolicConfiguration) -> Branching:
+        return Branching(smt.GE(self.current, self.end), StreamOperator.end, StreamOperator.not_end)
+
+    def end(self, config: SymbolicConfiguration) -> ChannelId(1):
+        self.current = None
+        self.end = None
+        self.transition_to(StreamOperator.start)
+        return smt.Int(1)
+    
+    def not_end(self, config: SymbolicConfiguration, step: ChannelId(2)) -> Tuple[ChannelId(0), ChannelId(1)]:
+        current = self.current
+        self.current = smt.Plus(self.current, step)
+        self.transition_to(StreamOperator.loop)
+        return current, smt.Int(0)
+
+    # def transition(self) -> TransitionGenerator:
+    #     if self.current is None:
+    #         first = yield PopChannelAction(0)
+    #         end = yield PopChannelAction(1)
+    #         step = yield PopChannelAction(2)
+
+    #         self.current = smt.Plus(first, step)
+    #         self.end = end
             
-            yield PushChannelAction(0, first)
-            yield PushChannelAction(1, smt.FALSE())
+    #         yield PushChannelAction(0, first)
+    #         yield PushChannelAction(1, smt.FALSE())
 
-        else:
-            step = yield PopChannelAction(2)
+    #     else:
+    #         step = yield PopChannelAction(2)
 
-            def true_branch(self) -> TransitionGenerator:
-                yield PushChannelAction(1, smt.TRUE())
-                self.current = None
-                self.end = None
+    #         def true_branch(self) -> TransitionGenerator:
+    #             yield PushChannelAction(1, smt.TRUE())
+    #             self.current = None
+    #             self.end = None
 
-            def false_branch(self) -> TransitionGenerator:
-                yield PushChannelAction(0, self.current)
-                yield PushChannelAction(1, smt.FALSE())
-                self.current = smt.Plus(self.current, step)
+    #         def false_branch(self) -> TransitionGenerator:
+    #             yield PushChannelAction(0, self.current)
+    #             yield PushChannelAction(1, smt.FALSE())
+    #             self.current = smt.Plus(self.current, step)
 
-            yield BranchAction(smt.GE(self.current, self.end), true_branch, false_branch)
+    #         yield BranchAction(smt.GE(self.current, self.end), true_branch, false_branch)
 
     def copy(self) -> StreamOperator:
         copied = super().copy()
@@ -118,17 +121,25 @@ class StreamOperator(Operator):
 
 @Operator.implement("MEM_CFG_OP_STORE")
 class StoreOperator(Operator):
-    def transition(self) -> TransitionGenerator:
-        base = yield PopChannelAction(0)
-        index = yield PopChannelAction(1)
-        value = yield PopChannelAction(2)
+    def __init__(self, *args):
+        super().__init__(*args)
+        
+        if len(self.pe.inputs) == 3:
+            self.transition_to(StoreOperator.start_3)
+        
+        elif len(self.pe.inputs) == 4:
+            self.transition_to(StoreOperator.start_4)
+        
+        else:
+            assert False, "unexpected number of input channels to the store operator"
 
-        # an additional synchronization signal
-        if len(self.pe.inputs) == 4:
-            yield PopChannelAction(3)
+    def start_3(self, config: SymbolicConfiguration, base: ChannelId(0), index: ChannelId(1), value: ChannelId(2)) -> ChannelId(0):
+        config.write_memory(base, index, value)
+        return smt.Int(1)
 
-        yield WriteMemoryAction(base, index, value)
-        yield PushChannelAction(0, smt.Int(1))
+    def start_4(self, config: SymbolicConfiguration, base: ChannelId(0), index: ChannelId(1), value: ChannelId(2), sync: ChannelId(3)) -> ChannelId(0):
+        config.write_memory(base, index, value)
+        return smt.Int(1)
 
 
 @dataclass
@@ -170,41 +181,31 @@ class SymbolicMemoryUpdate:
 
 
 @dataclass
-class TransitionState:
-    generator: TransitionGenerator
-    transcript: List[SymbolicMemoryUpdate]
-
-    def copy(self) -> TransitionState:
-        return TransitionState(self.generator, list(self.transcript))
-
-
-@dataclass
 class SymbolicConfiguration:
     operator_states: Tuple[Operator, ...]
     channel_states: Tuple[ChannelState, ...]
-    transition_states: List[Optional[TransitionState]]
-
     memory: List[SymbolicMemoryUpdate] = field(default_factory=list)
-    permission_constraints: List[permission.Formula] = field(default_factory=list)
 
     path_conditions: List[smt.SMTTerm] = field(default_factory=list)
+    permission_constraints: List[permission.Formula] = field(default_factory=list)
 
     def copy(self) -> SymbolicConfiguration:
         operator_states = tuple(operator.copy() for operator in self.operator_states)
         channel_states = tuple(state.copy() for state in self.channel_states)
-        transition_states = list(state.copy() if state is not None else None for state in self.transition_states)
         memory = list(self.memory)
-        permission_constraints = list(self.permission_constraints)
         path_conditions = list(self.path_conditions)
+        permission_constraints = list(self.permission_constraints)
 
         return SymbolicConfiguration(
             operator_states,
             channel_states,
-            transition_states,
             memory,
-            permission_constraints,
             path_conditions,
+            permission_constraints,
         )
+    
+    def write_memory(self, base: smt.SMTTerm, index: smt.SMTTerm, value: smt.SMTTerm):
+        self.memory.append(SymbolicMemoryUpdate(base, index, value))
 
 
 class SymbolicExecutor:
@@ -248,10 +249,7 @@ class SymbolicExecutor:
 
             channel_states.append(state)
 
-        # Initialize transition function states
-        transition_states: List[TransitionGenerator] = [None] * len(operator_impl)
-
-        init_config = SymbolicConfiguration(tuple(operator_impl), tuple(channel_states), transition_states)
+        init_config = SymbolicConfiguration(tuple(operator_impl), tuple(channel_states))
 
         self.configurations: List[SymbolicConfiguration] = [init_config]
 
@@ -270,7 +268,41 @@ class SymbolicExecutor:
 
             # true for sat, false for unsat
             return solver.solve()
+        
+    def get_transition_input_channels(self, pe_info: ProcessingElement, transition: TransitionFunction) -> Tuple[int, ...]:
+        signature = inspect.signature(transition, eval_str=True)
+        channel_ids = []
+        
+        for i, (name, channel_param) in enumerate(signature.parameters.items()):
+            if i == 0:
+                assert name == "self", f"ill-formed first transition parameter {channel_param}"
+            elif i == 1:
+                assert channel_param.annotation is SymbolicConfiguration, f"ill-formed second transition parameter {channel_param}"
+            else:
+                assert isinstance(channel_param.annotation, ChannelId), f"ill-formed transition parameter {channel_param}"
+                channel_id = pe_info.inputs[channel_param.annotation.id].id
+                channel_ids.append(channel_id)
+
+        return tuple(channel_ids)
     
+    def get_transition_output_actions(self, transition: TransitionFunction) -> Tuple[Any, ...]:
+        return_annotation = inspect.signature(transition, eval_str=True).return_annotation
+
+        if return_annotation is Branching:
+            return return_annotation,
+
+        elif isinstance(return_annotation, ChannelId):
+            return return_annotation,
+    
+        elif return_annotation is inspect.Signature.empty:
+            return ()
+        
+        elif "__origin__" in dir(return_annotation) and return_annotation.__origin__ is tuple:
+            return tuple(return_annotation.__args__)
+
+        else:
+            assert False, f"unrecognized transition return annotation {return_annotation}"
+
     def step(self, configuration: SymbolicConfiguration) -> Tuple[SymbolicConfiguration, ...]:
         """
         Try to proceed with each transition function until one of the following happens:
@@ -280,98 +312,78 @@ class SymbolicExecutor:
 
         changed = False
         
-        for pe_info, operator_state, transition_state in \
-            zip(self.graph.vertices, configuration.operator_states, configuration.transition_states):
-            if transition_state is not None:
-                blocking_action = transition_state.transcript[-1]
-                if isinstance(blocking_action, PopChannelAction):
-                
-                    channel_id = pe_info.inputs[blocking_action.input_index].id
-                    
-                    if configuration.channel_states[channel_id].ready():
-                        transition_state.generator.send(configuration.channel_states[channel_id].pop().term)
-                        changed = True
+        for pe_info, operator_state in \
+            zip(self.graph.vertices, configuration.operator_states):
 
-                    else:
-                        # not ready to execute the transition yet
+            transition = operator_state.current_transition
+            
+            input_channel_ids = self.get_transition_input_channels(pe_info, transition)
+            input_ready = False
+            
+            # Check for input channel availability
+            for channel_id in input_channel_ids:
+                if not configuration.channel_states[channel_id].ready():
+                    break
+            else:
+                input_ready = True
+
+            if not input_ready:
+                continue
+
+            # TODO: when channels are bounded, check for output channel availability here
+
+            changed = True
+
+            # Pop values from the input channels
+            input_values = []
+            for channel_id in input_channel_ids:
+                input_values.append(configuration.channel_states[channel_id].pop().term)
+
+            # Run the transition
+            output_values = transition(operator_state, configuration, *input_values)
+
+            # Process output behaviors
+            output_actions = self.get_transition_output_actions(transition)
+
+            if len(output_actions) == 0:
+                output_values = ()
+
+            elif len(output_actions) == 1 and not isinstance(output_values, tuple):
+                output_values = output_values,
+
+            assert len(output_values) == len(output_actions), f"output of transition {transition} does not match its specification"
+
+            for value, action in zip(output_values, output_actions):
+                if action is Branching:
+                    assert len(output_values) == 1, "cannot combine branching with other output actions"
+                    assert isinstance(value, Branching), f"output of transition {transition} does not match its specification"
+
+                    configuration_true = configuration
+                    configuration_false = configuration.copy()
+
+                    configuration_true.operator_states[pe_info.id].current_transition = value.true_branch
+                    configuration_true.path_conditions.append(value.condition)
+
+                    configuration_false.operator_states[pe_info.id].current_transition = value.false_branch
+                    configuration_false.path_conditions.append(smt.Not(value.condition))
+
+                    if not self.check_branch_feasibility(configuration_true):
+                        configuration = configuration_false
+                        # since the negation is unsat, the condition itself must be valid
+                        configuration.path_conditions.pop(-1)
                         continue
 
-                elif isinstance(blocking_action, BranchAction):
-                    pass
+                    if not self.check_branch_feasibility(configuration_false):
+                        configuration = configuration_true
+                        configuration.path_conditions.pop(-1)
+                        continue
 
-                else:
-                    assert False, f"unexpected blocking action {blocking_action}"
-
-            else:
-                transition_state = TransitionState(operator_state.transition(), [])
-                configuration.transition_states[pe_info.id] = transition_state
-                changed = True
-
-            # print(pe_info)
-
-            # try to continue executing the transition function
-            send_value = None
-
-            while True:
-                try:
-                    action = transition_state.generator.send(send_value)
-                    transition_state.transcript.append(action)
-                    send_value = None
-
-                    if isinstance(action, PopChannelAction):
-                        channel_id = pe_info.inputs[action.input_index].id
-                        channel_state = configuration.channel_states[channel_id]
-                        if channel_state.ready():
-                            send_value = channel_state.pop().term
-                        else:
-                            # not ready, we are stuck on this operator
-                            break
-
-                    elif isinstance(action, PushChannelAction):
-                        if action.output_index in pe_info.outputs:
-                            channels = pe_info.outputs[action.output_index]
-                            
-                            for channel in channels:
-                                channel_state = configuration.channel_states[channel.id]
-                                channel_state.push(PermissionedValue(action.value, self.get_fresh_permission_var()))
-
-                    elif isinstance(action, ReadMemoryAction):
-                        raise NotImplementedError()
-
-                    elif isinstance(action, WriteMemoryAction):
-                        configuration.memory.append(SymbolicMemoryUpdate(action.base, action.index, action.value))
-
-                    elif isinstance(action, BranchAction):
-                        configuration_true = configuration
-                        configuration_false = configuration.copy()
-
-                        transition_state_true = TransitionState(action.true_branch(configuration_true.operator_states[pe_info.id]), transition_state.transcript)
-                        configuration_true.transition_states[pe_info.id] = transition_state_true
-                        configuration_true.path_conditions.append(action.condition)
-
-                        transition_state_false = TransitionState(action.false_branch(configuration_false.operator_states[pe_info.id]), transition_state.transcript)
-                        configuration_false.transition_states[pe_info.id] = transition_state_false
-                        configuration_false.path_conditions.append(smt.Not(action.condition))
-
-                        if not self.check_branch_feasibility(configuration_true):
-                            configuration = configuration_false
-                            transition_state = transition_state_false
-                            continue
-
-                        if not self.check_branch_feasibility(configuration_false):
-                            configuration = configuration_true
-                            transition_state = transition_state_true
-                            continue
-
-                        return configuration_true, configuration_false
-
-                    else:
-                        assert False, f"unsupported action {action}"
+                    return configuration_true, configuration_false
                 
-                except StopIteration:
-                    # TODO: accumulate new permission constraints
-                    configuration.transition_states[pe_info.id] = None
-                    break
+                elif isinstance(action, ChannelId) and action.id in pe_info.outputs:
+                    channels = pe_info.outputs[action.id]
+                    for channel in channels:
+                        configuration.channel_states[channel.id].push(self.get_fresh_permissioned_value(value))
 
         if not changed:
             return ()

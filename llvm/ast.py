95954bad82da381f11ec7d3c8afb8d4168e79582
Zhengyao Lin <zhengyao-lin@outlook.com>
2023-09-18 11:46:46 -0400

    Finish first pass of the parser

--- llvm/ast.py
+++ llvm/ast.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
+from typing import Tuple, Optional
 
 from collections import OrderedDict
-from typing import Tuple
 from dataclasses import dataclass
 
 
@@ -11,10 +11,15 @@ class ASTNode:
 
 @dataclass
 class Module(ASTNode):
-    ...
+    functions: Tuple[Function, ...]
 
 
 class Type(ASTNode):
+    ...
+
+
+@dataclass
+class VoidType(Type):
     ...
 
 
@@ -36,6 +41,8 @@ class FunctionParameter(ASTNode):
 
 @dataclass
 class Function(ASTNode):
+    name: str
+    return_type: str
     parameters: Tuple[FunctionParameter, ...]
     blocks: OrderedDict[str, BasicBlock]
 
@@ -49,6 +56,36 @@ class BasicBlock(ASTNode):
 class Value(ASTNode):
     def get_type(self) -> Type:
         raise NotImplementedError()
+
+
+# Values who types cannot be inferred yet
+class UnresolvedValue(Value):
+    def attach_type(self, typ: Type) -> Value:
+        raise NotImplementedError()
+
+@dataclass
+class UnresolvedIntegerValue(UnresolvedValue):
+    value: int
+
+    def attach_type(self, typ: Type) -> IntegerConstant:
+        assert isinstance(typ, IntegerType)
+        return IntegerConstant(typ, self.value)
+
+
+class UnresolvedNullValue(UnresolvedValue):
+    def attach_type(self, typ: Type) -> NullConstant:
+        assert isinstance(typ, PointerType)
+        return NullConstant(typ.base_type)
+
+
+# Unresolved variable use
+@dataclass
+class UnresolvedVariable(UnresolvedValue):
+    name: str
+    type: Optional[Type] = None
+
+    def attach_type(self, typ: Type) -> UnresolvedVariable:
+        return UnresolvedVariable(self.name, typ)
 
 
 class Constant(Value):
@@ -72,6 +109,77 @@ class Instruction(Value):
 
 @dataclass
 class AddInstruction(Instruction):
+    name: str
     type: Type
     left: Value
     right: Value
+
+
+@dataclass
+class MulInstruction(Instruction):
+    name: str
+    type: Type
+    left: Value
+    right: Value
+
+
+@dataclass
+class IntegerCompareInstruction(Instruction):
+    name: str
+    cond: str
+    type: Type
+    left: Value
+    right: Value
+
+
+@dataclass
+class GetElementPointerInstruction(Instruction):
+    name: str
+    base_type: Type
+    pointer: Value
+    indices: Tuple[Value, ...]
+
+
+@dataclass
+class LoadInstruction(Instruction):
+    name: str
+    base_type: Type
+    pointer: Value
+
+
+@dataclass
+class StoreInstruction(Instruction):
+    base_type: Type
+    value: Value
+    dest: Value
+
+
+@dataclass
+class PhiInstruction(Instruction):
+    name: str
+    type: Type
+    branches: Tuple[PhiBranch, ...]
+
+
+@dataclass
+class PhiBranch(ASTNode):
+    value: Value
+    label: str
+
+
+@dataclass
+class JumpInstruction(Instruction):
+    label: str
+
+
+@dataclass
+class BranchInstruction(Instruction):
+    cond: Value
+    true_label: str
+    false_label: str
+
+
+@dataclass
+class ReturnInstruction(Instruction):
+    type: Type
+    value: Optional[Value] = None
--- llvm/parser.py
+++ llvm/parser.py
@@ -1,4 +1,120 @@
+from typing import List, Any, Union, Tuple, Optional
+
 from lark import Lark, Transformer, Token, Tree
+from .ast import (
+    ASTNode, Module, Function,
+    BasicBlock,
+    Type, VoidType, IntegerType, PointerType, FunctionParameter,
+    UnresolvedValue, UnresolvedIntegerValue, UnresolvedNullValue, UnresolvedVariable,
+    Value,
+    Instruction, AddInstruction, MulInstruction, IntegerCompareInstruction,
+    GetElementPointerInstruction, LoadInstruction, StoreInstruction,
+    PhiInstruction, PhiBranch,
+    JumpInstruction, BranchInstruction, ReturnInstruction,
+)
+
+
+class ASTTransformer(Transformer[ASTNode]):
+    # Ignore all metadata for now
+    def metadata(self, args: List[Any]) -> None:
+        return None
+
+    def label(self, args: List[Token]) -> str:
+        return args[0].value
+
+    def module(self, args: List[Optional[Function]]) -> Module:
+        return Module(tuple(arg for arg in args if arg is not None))
+
+    def function(self, args: List[Any]) -> Function:
+        _, return_typ, name_token, parameters, _, blocks = args
+        return Function(name_token.value, return_typ, parameters, blocks)
+
+    def basic_blocks(self, args: List[BasicBlock]) -> Tuple[BasicBlock, ...]:
+        return tuple(args)
+
+    def basic_block(self, args: List[Any]) -> BasicBlock:
+        label = args[0].value[:-1]
+        return BasicBlock(label, tuple(args[1:]))
+
+    def instruction_with_metadata(self, args: List[Any]) -> Instruction:
+        return args[0] # ignoring metadata
+
+    def instruction(self, args: List[Instruction]) -> Instruction:
+        return args[0]
+    
+    def add_instruction(self, args: List[Any]) -> AddInstruction:
+        typ = args[1]
+        return AddInstruction(args[0].name, typ, args[2].attach_type(typ), args[3].attach_type(typ))
+    
+    def mul_instruction(self, args: List[Any]) -> MulInstruction:
+        typ = args[1]
+        return MulInstruction(args[0].name, typ, args[2].attach_type(typ), args[3].attach_type(typ))
+
+    def icmp_instruction(self, args: List[Any]) -> IntegerCompareInstruction:
+        typ = args[2]
+        return IntegerCompareInstruction(args[0].name, args[1].value, typ, args[3].attach_type(typ), args[4].attach_type(typ))
+
+    def getelementptr_index(self, args: List[Any]) -> Value:
+        return args[1].attach_type(args[0])
+
+    def getelementptr_instruction(self, args: List[Any]) -> GetElementPointerInstruction:
+        return GetElementPointerInstruction(args[0].name, args[1], args[3].attach_type(args[2]), tuple(args[4:]))
+
+    def load_instruction(self, args: List[Any]) -> LoadInstruction:
+        return LoadInstruction(args[0].name, args[1], args[3].attach_type(args[2]))
+    
+    def store_instruction(self, args: List[Any]) -> StoreInstruction:
+        return StoreInstruction(args[0], args[1].attach_type(args[0]), args[3].attach_type(args[2]))
+
+    def phi_instruction_branch(self, args: List[Any]) -> PhiBranch:
+        return PhiBranch(args[0], args[1])
+
+    def phi_instruction(self, args: List[Any]) -> PhiInstruction:
+        return PhiInstruction(
+            args[0],
+            args[1],
+            tuple(PhiBranch(branch.value.attach_type(args[1]), branch.label) for branch in args[2:]),
+        )
+    
+    def jump_instruction(self, args: List[Any]) -> JumpInstruction:
+        return JumpInstruction(args[0])
+    
+    def br_instruction(self, args: List[Any]) -> BranchInstruction:
+        return BranchInstruction(args[0].attach_type(IntegerType(1)), args[1], args[2])
+    
+    def ret_instruction(self, args: List[Any]) -> ReturnInstruction:
+        return ReturnInstruction(args[0], args[1].attach_type(args[0]) if len(args) == 2 else None)
+
+    def parameters(self, args: List[FunctionParameter]) -> Tuple[FunctionParameter, ...]:
+        return tuple(args)
+
+    def parameter(self, args: List[Any]) -> List[FunctionParameter]:
+        return FunctionParameter(args[0], args[2].value)
+
+    def type(self, args: List[Type]) -> Type:
+        return args[0]
+    
+    def void_type(self, args: List[Token]) -> VoidType:
+        return VoidType()
+
+    def integer_type(self, args: List[Token]) -> IntegerType:
+        return IntegerType(int(args[0].value[1:]))
+
+    def pointer_type(self, args: List[Type]) -> PointerType:
+        return PointerType(args[0])
+
+    def value(self, args: List[UnresolvedValue]) -> UnresolvedValue:
+        return args[0]
+    
+    def variable(self, args: List[Token]) -> UnresolvedVariable:
+        return UnresolvedVariable(args[0].value)
+
+    def integer_value(self, args: List[Token]) -> UnresolvedIntegerValue:
+        return UnresolvedIntegerValue(int(args[0].value))
+    
+    def null_value(self, args: List[Token]) -> UnresolvedNullValue:
+        return UnresolvedNullValue()
+
 
 class Parser:
     SYNTAX = r"""
@@ -19,33 +135,76 @@ class Parser:
 
         module: (metadata|function)*
 
-        type: "void"
-            | INTEGER_TYPE
-            | type "*"
+        type: "void" -> void_type
+            | integer_type
+            | pointer_type
 
-        function: "define" function_attribute* type VARIABLE "(" [parameter ("," parameter)*] ")" function_attribute* "{" basic_block* "}"
+        integer_type: INTEGER_TYPE
+        pointer_type: type "*"
 
-        basic_block: BLOCK_LABEL instruction*
+        function: "define" function_attributes type VARIABLE "(" parameters ")" function_attributes "{" basic_blocks "}"
+        
+        parameters: [parameter ("," parameter)*]
+        parameter: type parameter_attributes VARIABLE
+        
+        basic_blocks: basic_block*
+        basic_block: BLOCK_LABEL instruction_with_metadata*
 
-        register: VARIABLE
-
-        value: INTEGER_TYPE INTEGER
-             | type "*" "null"
-             | register
-
+        instruction_with_metadata: instruction ("," METADATA_LABEL+)*
+        
         instruction: add_instruction
+                   | mul_instruction
+                   | icmp_instruction
+                   | getelementptr_instruction
+                   | load_instruction
+                   | store_instruction
+                   | phi_instruction
+                   | jump_instruction
+                   | br_instruction
                    | ret_instruction
 
-        add_instruction: register "=" "add" type value "," value
+        add_instruction: variable "=" "add" integer_type value "," value
+        mul_instruction: variable "=" "mul" integer_type value "," value
 
+        icmp_instruction: variable "=" "icmp" ICMP_CONDITION type value "," value
+        ICMP_CONDITION.1: /eq|ne|ugt|uge|ult|ule|sgt|sge|slt|sle/
+ 
+        getelementptr_instruction: variable "=" "getelementptr" ["inbounds"] type "," pointer_type value ("," ["inrange"] getelementptr_index)*
+        getelementptr_index: integer_type value
+
+        load_instruction: variable "=" "load" type "," pointer_type value ["," "align" INTEGER]
+
+        store_instruction: "store" type value "," pointer_type value ["," "align" INTEGER]
+
+        phi_instruction: variable "=" "phi" type phi_instruction_branch ("," phi_instruction_branch)*
+        phi_instruction_branch: "[" value "," label "]"
+        
+        jump_instruction: "br" "label" label
+
+        br_instruction: "br" "i1" value "," "label" label "," "label" label
+        
         ret_instruction: "ret" type [value]
 
-        parameter: type parameter_attribute* VARIABLE
+        label: VARIABLE
+        variable: VARIABLE
+
+        value: INTEGER -> integer_value
+             | "null" -> null_value
+             | variable
+
+        /////////////////////////////////////////////////////
+        // The portion of the syntax below will be ignored //
+        // in our AST.                                     //
+        /////////////////////////////////////////////////////
+
+        function_attributes: function_attribute*
 
         function_attribute: ATTRIBUTE_GROUP_NAME
                           | "dso_local"
                           | "local_unnamed_addr"
 
+        parameter_attributes: parameter_attribute*
+                          
         parameter_attribute: "noalias"
                            | "nocapture"
                            | "readonly"
@@ -80,13 +239,14 @@ class Parser:
     )
 
     @staticmethod
-    def parse_module(src: str):
-        print(Parser.MODULE_PARSER.parse(src))
+    def parse_module(src: str) -> Module:
+        raw_ast = Parser.MODULE_PARSER.parse(src)
+        return ASTTransformer().transform(raw_ast)
 
 
 if __name__ == "__main__":
-    Parser.parse_module(r"""
-; ModuleID = 'dmv.c'
+    module = Parser.parse_module(r"""
+; ModuleID = 'dmv.base.ll'
 source_filename = "dmv.c"
 target datalayout = "e-m:o-p:32:32-Fi8-f64:32:64-v64:32:64-v128:32:128-a:0:32-n32-S32"
 target triple = "armv4t-apple-macosx12.0.0"
@@ -94,11 +254,47 @@ target triple = "armv4t-apple-macosx12.0.0"
 ; Function Attrs: minsize nofree norecurse nounwind optsize ssp
 define dso_local void @dmv(i16* noalias nocapture readonly %A, i16* noalias nocapture readonly %B, i16* noalias nocapture %Z, i32 %m, i32 %n) local_unnamed_addr #0 {
 entry:
+  br label %for.cond
+
+for.cond:                                         ; preds = %for.cond.cleanup3, %entry
+  %i.0 = phi i32 [ 0, %entry ], [ %inc13, %for.cond.cleanup3 ]
+  %exitcond26.not = icmp eq i32 %i.0, %m
+  br i1 %exitcond26.not, label %for.cond.cleanup, label %for.cond1.preheader
+
+for.cond1.preheader:                              ; preds = %for.cond
+  %mul = mul i32 %i.0, %n
+  br label %for.cond1
+
+for.cond.cleanup:                                 ; preds = %for.cond
   ret void
+
+for.cond1:                                        ; preds = %for.body4, %for.cond1.preheader
+  %w.0 = phi i16 [ %add9, %for.body4 ], [ 0, %for.cond1.preheader ]
+  %j.0 = phi i32 [ %inc, %for.body4 ], [ 0, %for.cond1.preheader ]
+  %exitcond.not = icmp eq i32 %j.0, %n
+  br i1 %exitcond.not, label %for.cond.cleanup3, label %for.body4
+
+for.cond.cleanup3:                                ; preds = %for.cond1
+  %w.0.lcssa = phi i16 [ %w.0, %for.cond1 ]
+  %arrayidx11 = getelementptr inbounds i16, i16* %Z, i32 %i.0
+  store i16 %w.0.lcssa, i16* %arrayidx11, align 2, !tbaa !4
+  %inc13 = add i32 %i.0, 1
+  br label %for.cond, !llvm.loop !8
+
+for.body4:                                        ; preds = %for.cond1
+  %add = add i32 %j.0, %mul
+  %arrayidx = getelementptr inbounds i16, i16* %A, i32 %add
+  %0 = load i16, i16* %arrayidx, align 2, !tbaa !4
+  %arrayidx5 = getelementptr inbounds i16, i16* %B, i32 %j.0
+  %1 = load i16, i16* %arrayidx5, align 2, !tbaa !4
+  %mul7 = mul i16 %1, %0
+  %add9 = add i16 %mul7, %w.0
+  %inc = add i32 %j.0, 1
+  br label %for.cond1, !llvm.loop !10
 }
 
 attributes #0 = { minsize nofree norecurse nounwind optsize ssp "disable-tail-calls"="false" "frame-pointer"="all" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="arm7tdmi" "target-features"="+armv4t,+soft-float,+strict-align,-bf16,-crypto,-d32,-dotprod,-fp-armv8,-fp-armv8d16,-fp-armv8d16sp,-fp-armv8sp,-fp16,-fp16fml,-fp64,-fpregs,-fullfp16,-mve,-mve.fp,-neon,-thumb-mode,-vfp2,-vfp2sp,-vfp3,-vfp3d16,-vfp3d16sp,-vfp3sp,-vfp4,-vfp4d16,-vfp4d16sp,-vfp4sp" "unsafe-fp-math"="false" "use-soft-float"="true" }
-                        
+
 !llvm.module.flags = !{!0, !1, !2}
 !llvm.ident = !{!3}
 
@@ -113,6 +309,7 @@ attributes #0 = { minsize nofree norecurse nounwind optsize ssp "disable-tail-ca
 !8 = distinct !{!8, !9}
 !9 = !{!"llvm.loop.mustprogress"}
 !10 = distinct !{!10, !9}
-
 """)
+                                 
+    print(module)
 

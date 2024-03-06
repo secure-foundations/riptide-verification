c23675b20f38b5ff58e22da6237e3e5d0be19cc8
Zhengyao Lin <zhengyao-lin@outlook.com>
2024-03-06 13:58:39 -0500

    WIP tools/mc.py

--- tools/mc.py
+++ tools/mc.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 from collections import OrderedDict
 
 import json
+import time
 import argparse
 
 from semantics.dataflow import *
@@ -11,26 +12,29 @@ from semantics.dataflow import *
 
 @dataclass(frozen=True)
 class ConfigurationShape:
+    graph: DataflowGraph
     operator_transitions: Tuple[TransitionFunction, ...]
     channel_size: Tuple[int, ...]
 
     @staticmethod
     def from_config(config: Configuration) -> ConfigurationShape:
         return ConfigurationShape(
+            config.graph,
             tuple(op_state.current_transition for op_state in config.operator_states),
             tuple(channel_state.count() if not channel_state.hold_constant else 0 for channel_state in config.channel_states),
         )
 
 
-"""
-Maintains a partition of configurations by their shape
-"""
 @dataclass
 class ShapePartition:
+    """
+    Maintains a partition of configurations by their shape
+    """
+
     shape_map: OrderedDict[ConfigurationShape, List[Configuration]] = field(default_factory=OrderedDict)
 
-    def __contains__(self, config: Configuration) -> bool:
-        return ConfigurationShape.from_config(config) in self.shape_map
+    def __contains__(self, shape: ConfigurationShape) -> bool:
+        return shape in self.shape_map
 
     def add(self, config: Configuration):
         shape = ConfigurationShape.from_config(config)
@@ -50,6 +54,80 @@ class ShapePartition:
 
     def shapes(self) -> Iterable[ConfigurationShape]:
         return self.shape_map.keys()
+
+
+@dataclass
+class ShapeGraph:
+    """
+    A directed graph storing the transition relation between shapes
+    """
+
+    shapes: List[ConfigurationShape] = field(default_factory=list)
+    shape_to_index: Dict[ConfigurationShape, int] = field(default_factory=dict)
+
+    incoming_edges: Dict[int, Set[int]] = field(default_factory=dict)
+    outgoing_edges: Dict[int, Set[int]] = field(default_factory=dict)
+
+    def add_shape(self, shape: ConfigurationShape):
+        if shape not in self.shape_to_index:
+            self.shape_to_index[shape] = len(self.shapes)
+            self.shapes.append(shape)
+
+    def get_shapes(self) -> Iterable[ConfigurationShape]:
+        return self.shapes
+
+    def get_incoming_shapes(self, shape: ConfigurationShape) -> Tuple[ConfigurationShape]:
+        shape_index = self.shape_to_index[shape]
+        indices = sorted(self.incoming_edges.get(shape_index, set()))
+        return tuple(self.shapes[index] for index in indices)
+
+    def get_outgoing_shapes(self, shape: ConfigurationShape) -> Tuple[ConfigurationShape]:
+        shape_index = self.shape_to_index[shape]
+        indices = sorted(self.outgoing_edges.get(shape_index, set()))
+        return tuple(self.shapes[index] for index in indices)
+
+    def add_edge(self, shape1: ConfigurationShape, shape2: ConfigurationShape):
+        shape_index1 = self.shape_to_index[shape1]
+        shape_index2 = self.shape_to_index[shape2]
+
+        if shape_index2 not in self.incoming_edges:
+            self.incoming_edges[shape_index2] = set()
+
+        if shape_index1 not in self.outgoing_edges:
+            self.outgoing_edges[shape_index1] = set()
+
+        # assert shape_index1 not in self.incoming_edges[shape_index2]
+        # assert shape_index2 not in self.outgoing_edges[shape_index1]
+
+        self.incoming_edges[shape_index2].add(shape_index1)
+        self.outgoing_edges[shape_index1].add(shape_index2)
+
+
+@dataclass
+class FirebilityDependencyGraph:
+    """
+    Fix a configuration (shape) C
+
+    Firebility dependency graph is a directed graph where nodes are PEs
+    And a -> b iff a in C is fireable only after b is fired
+    """
+
+    graph: DataflowGraph
+    dependency: Dict[int, Set[int]] = field(default_factory=dict)
+
+    def add_dependencies(self, pe_id1: int, pe_id2: int):
+        if pe_id1 not in self.dependency:
+            self.dependency[pe_id1] = set()
+        self.dependency[pe_id1].add(pe_id2)
+
+    @staticmethod
+    def from_shape(shape: ConfigurationShape) -> FirebilityDependencyGraph:
+        ...
+
+    def find_loop(self) -> Tuple[int, ...]:
+        """
+        Find one loop in the graph
+        """
 
 
 """
@@ -106,7 +184,7 @@ def explore_states(
     initial: Configuration,
     schedule: OperatorSchedule,
     max_depth: int,
-) -> Tuple[Configuration]:
+) -> Tuple[Tuple[Configuration], ShapePartition]:
     """
     Explore the tree of symbolic states from an initial configuration,
     up to a certain depth
@@ -118,32 +196,101 @@ def explore_states(
 
     max_branching: int = 1
 
-    # BFS on the symbolic configurations
-    while len(queue) != 0:
-        config, depth = queue.pop(0)
-        explored_states.append(config)
+    partitions = ShapePartition()
+    shape_graph = ShapeGraph()
 
-        if depth >= max_depth:
-            continue
+    symbolic_execution_time = 0.0
+    start_time = time.time()
 
-        operators_to_fire = schedule(config)
+    try:
+        # BFS on the symbolic configurations
+        while len(queue) != 0:
+            config, depth = queue.pop(0)
+            explored_states.append(config)
 
-        num_branches = 0
+            shape = ConfigurationShape.from_config(config)
+            shape_graph.add_shape(shape)
 
-        for pe_id in operators_to_fire:
-            results = config.copy().step_exhaust(pe_id)
+            partitions.add(config)
 
-            for result in results:
-                assert isinstance(result, NextConfiguration)
-                queue.append((result.config, depth + 1))
-                num_branches += 1
+            if len(explored_states) % 100 == 0:
+                num_redundant = 0
+                num_final = 0
+                num_initial = 0
+                num_branching = 0
+                num_merging = 0
+                num_other = 0
 
-        if num_branches > max_branching:
-            max_branching = num_branches
+                other_fan_ins = []
+                other_fan_outs = []
+
+                for explored_shape in shape_graph.get_shapes():
+                    if (len(shape_graph.get_incoming_shapes(explored_shape)) == 1 and
+                        len(shape_graph.get_outgoing_shapes(explored_shape)) == 1):
+                        num_redundant += 1
+
+                    elif len(shape_graph.get_outgoing_shapes(explored_shape)) == 0:
+                        num_final += 1
+
+                    elif len(shape_graph.get_incoming_shapes(explored_shape)) == 0:
+                        num_initial += 1
+
+                    elif (len(shape_graph.get_incoming_shapes(explored_shape)) == 1 and
+                        len(shape_graph.get_outgoing_shapes(explored_shape)) > 1):
+                        num_branching += 1
+
+                    elif (len(shape_graph.get_incoming_shapes(explored_shape)) > 1 and
+                        len(shape_graph.get_outgoing_shapes(explored_shape)) == 1):
+                        num_merging += 1
+
+                    else:
+                        num_other += 1
+                        other_fan_ins.append(len(shape_graph.get_incoming_shapes(explored_shape)))
+                        other_fan_outs.append(len(shape_graph.get_outgoing_shapes(explored_shape)))
+
+                print(
+                    f"{len(explored_states)} state(s) explored, depth {depth}, "
+                    f"{len(partitions)} partition(s) found ({num_redundant} redundant, {num_final} final, {num_initial} initial, {num_branching} branching, {num_merging} merging, {num_other} other), "
+                    f"symbolic execution time {round(symbolic_execution_time, 2)} s, "
+                    f"total time {round(time.time() - start_time, 2)} s"
+                )
+                print(*zip(other_fan_ins, other_fan_outs))
+
+            if depth >= max_depth:
+                continue
+
+            operators_to_fire = schedule(config)
+
+            num_branches = 0
+
+            for pe_id in operators_to_fire:
+                start = time.time()
+                results = config.copy().step_exhaust(pe_id)
+                symbolic_execution_time += time.time() - start
+
+                for result in results:
+                    assert isinstance(result, NextConfiguration)
+
+                    result_shape = ConfigurationShape.from_config(result.config)
+                    if result_shape not in partitions:
+                        shape_graph.add_shape(result_shape)
+                    shape_graph.add_edge(shape, result_shape)
+
+                    queue.append((result.config, depth + 1))
+                    num_branches += 1
+
+            if num_branches > max_branching:
+                max_branching = num_branches
+
+    except:
+        print("final state (examples):")
+        # for explored_shape in shape_graph.get_shapes():
+        #     if len(shape_graph.get_outgoing_shapes(explored_shape)) == 0 and explored_shape in partitions:
+        #         print(partitions.shape_map[explored_shape][0])
 
     print("max branching", max_branching)
 
-    return tuple(explored_states)
+    return tuple(explored_states), partitions
 
 
 def generalize_partition(partition: Tuple[Configuration, ...]) -> Configuration:
@@ -200,6 +347,42 @@ def generalize_partition(partition: Tuple[Configuration, ...]) -> Configuration:
 
             unique_value = values_in_partition[0]
 
+            # could_be_non_zero = False
+            # could_be_non_one = False
+
+            # for config in partition:
+            #     value = config.channel_states[channel.id].values[index].term
+
+            #     with smt.push_solver(config.solver):
+            #         for condition in config.path_conditions:
+            #             config.solver.add_assertion(condition)
+
+            #         with smt.push_solver(config.solver):
+            #             config.solver.add_assertion(smt.Not(smt.Equals(value, smt.BVConst(0, WORD_WIDTH))))
+
+            #             if config.solver.solve():
+            #                 could_be_non_zero = True
+
+            #         with smt.push_solver(config.solver):
+            #             config.solver.add_assertion(smt.Not(smt.Equals(value, smt.BVConst(1, WORD_WIDTH))))
+
+            #             if config.solver.solve():
+            #                 could_be_non_one = True
+
+            #         if could_be_non_zero and could_be_non_one:
+            #             break
+
+            # assert could_be_non_zero or could_be_non_one
+
+            # if not could_be_non_zero:
+            #     value = smt.BVConst(0, WORD_WIDTH)
+
+            # elif not could_be_non_one:
+            #     value = smt.BVConst(1, WORD_WIDTH)
+
+            # else:
+            #     value = smt.FreshSymbol(smt.BVType(WORD_WIDTH), f"cut_point_channel_{channel.id}_{index}_%d")
+
             # if len(set(values_in_partition)):
             #     print(unique_value.get_free_variables(), unique_value.get_free_variables().issubset(function_arg_free_vars))
 
@@ -229,6 +412,76 @@ def generalize_partition(partition: Tuple[Configuration, ...]) -> Configuration:
                         # print(smt.Equals(value1.term, value2.term))
                         cut_point.path_conditions.append(smt.Equals(value1.term, value2.term))
 
+    # Learn some potential equality constraints
+    # for i, channel1 in enumerate(cut_point.graph.channels):
+    #     for channel2 in cut_point.graph.channels[i + 1:]:
+    #         for j, value1 in enumerate(cut_point.channel_states[channel1.id].values):
+    #             for k, value2 in enumerate(cut_point.channel_states[channel2.id].values):
+    #                 for config in partition:
+    #                     if config.channel_states[channel1.id].values[j].term != config.channel_states[channel2.id].values[k].term:
+    #                         break
+    #                 else:
+    #                     # All values in these two sets have the same value (syntactically)
+    #                     # so infer an equality between them
+    #                     cut_point.path_conditions.append(smt.Equals(value1.term, value2.term))
+
+    # Infer some constraints about whether a value has to be zero or non-zero
+    for channel in cut_point.graph.channels:
+        if channel.hold:
+            continue
+
+        for index in range(cut_point.channel_states[channel.id].count()):
+
+            cut_point_value = cut_point.channel_states[channel.id].values[index].term
+
+            all_zeroes = True
+            all_non_zeroes = True
+
+            for config in partition:
+                value = config.channel_states[channel.id].values[index].term
+
+                with smt.push_solver(config.solver):
+                    for condition in config.path_conditions:
+                        config.solver.add_assertion(condition)
+
+                    if all_zeroes:
+                        with smt.push_solver(config.solver):
+                            config.solver.add_assertion(smt.Not(smt.Equals(value, smt.BVConst(0, WORD_WIDTH))))
+
+                            if config.solver.solve():
+                                all_zeroes = False
+                            else:
+                                all_non_zeroes = False
+
+                    if all_non_zeroes:
+                        with smt.push_solver(config.solver):
+                            config.solver.add_assertion(smt.Equals(value, smt.BVConst(0, WORD_WIDTH)))
+
+                            if config.solver.solve():
+                                all_non_zeroes = False
+                            else:
+                                all_zeroes = False
+
+                if not all_zeroes and not all_non_zeroes:
+                    break
+
+            assert not (all_zeroes and all_non_zeroes)
+
+            if all_zeroes:
+                cut_point.path_conditions.append(smt.Equals(cut_point_value, smt.BVConst(0, WORD_WIDTH)))
+
+            elif all_non_zeroes:
+                cut_point.path_conditions.append(smt.Not(smt.Equals(cut_point_value, smt.BVConst(0, WORD_WIDTH))))
+
+    # for config in partition:
+    #     match_result, _ = cut_point.match(config)
+    #     assert isinstance(match_result, MatchingSuccess)
+
+    #     if not match_result.check_condition():
+    #         print(cut_point, config)
+    #         print(match_result.condition)
+    #         exit()
+
     return cut_point
 
 
@@ -236,6 +489,7 @@ def construct_cut_point_abstraction(
     # cut_points: Tuple[Configuration, ...],
     configs: Tuple[Configuration, ...],
     schedule: OperatorSchedule,
+    exact_partitions: ShapePartition,
     # offset: int = 0,
 ) -> Tuple[Configuration, ...]:
     """
@@ -271,6 +525,12 @@ def construct_cut_point_abstraction(
                 for result in results:
                     assert isinstance(result, NextConfiguration)
 
+                    # if (ConfigurationShape.from_config(result.config) not in exact_partitions and
+                    #     ConfigurationShape.from_config(cut_point) in exact_partitions):
+                    #     print(cut_point)
+                    #     print(result.config)
+                    #     exit()
+
                     for other_cut_point in cut_points.values():
                         match_result, _ = other_cut_point.match(result.config)
                         if isinstance(match_result, MatchingSuccess) and match_result.check_condition():
@@ -295,7 +555,7 @@ def construct_cut_point_abstraction(
 
             for config in unmatched_configs:
                 shape = ConfigurationShape.from_config(config)
-                if config not in partitions:
+                if shape not in partitions:
                     num_new += 1
 
                 updated_shapes[shape] = None
@@ -389,6 +649,14 @@ def construct_cut_point_abstraction(
     #     return cut_points
 
 
+def check_deadlock(shape: ConfigurationShape) -> Tuple[int, ...]:
+    """
+    Return which operators are in a deadlock
+    """
+
+
+
+
 function_arg_free_vars: Set[smt.SMTTerm] = set()
 
 def main():
@@ -407,43 +675,47 @@ def main():
             free_vars[function_arg.variable_name] = smt.FreshSymbol(smt.BVType(WORD_WIDTH))
             function_arg_free_vars.add(free_vars[function_arg.variable_name])
 
-    initial = Configuration.get_initial_configuration(dfg, free_vars, disable_permissions=True)
+    with smt.Solver("z3") as solver:
+        initial = Configuration.get_initial_configuration(dfg, free_vars, disable_permissions=True, solver=solver)
 
-    # configs = explore_states(initial, pure_priority_schedule, 200)
+        # partitions: List[List[Configuration]] = []
 
-    # partitions: List[List[Configuration]] = []
+        # # TODO: inefficient partition algo
+        # for state in configs:
+        #     for partition in partitions:
+        #         if partition_relation(state, partition[0]):
+        #             partition.append(state)
+        #             break
+        #     else:
+        #         partitions.append([state])
 
-    # # TODO: inefficient partition algo
-    # for state in configs:
-    #     for partition in partitions:
-    #         if partition_relation(state, partition[0]):
-    #             partition.append(state)
-    #             break
-    #     else:
-    #         partitions.append([state])
+        # for state in states:
+        #     print(state)
 
-    # for state in states:
-    #     print(state)
+        # sorted_partitions = sorted(partitions, key=lambda p: len(p), reverse=True)
+        # for config in sorted_partitions[0]:
+        #     print(config)
 
-    # sorted_partitions = sorted(partitions, key=lambda p: len(p), reverse=True)
-    # for config in sorted_partitions[0]:
-    #     print(config)
+        # print(len(configs), len(partitions))
 
-    # print(len(configs), len(partitions))
+        # cut_points = tuple(generalize_partition(partition) for partition in partitions)
 
-    # cut_points = tuple(generalize_partition(partition) for partition in partitions)
+        # # cut_points.pop(3)
+        # # cut_points.pop(2)
+        # # cut_points.pop(1)
 
-    # # cut_points.pop(3)
-    # # cut_points.pop(2)
-    # # cut_points.pop(1)
+        # cut_points = construct_cut_point_abstraction(cut_points, pure_priority_schedule)
 
-    # cut_points = construct_cut_point_abstraction(cut_points, pure_priority_schedule)
+        # print(f"graph size: {len(dfg.vertices)} operator(s), {len(dfg.channels)} channel(s)")
 
-    # print(f"graph size: {len(dfg.vertices)} operator(s), {len(dfg.channels)} channel(s)")
+        # _, partitions = explore_states(initial, pure_priority_schedule, 1000)
 
-    cut_points = construct_cut_point_abstraction([initial], pure_priority_schedule)
+        # for part in partitions.shape_map.values():
+        #     print(part[0])
 
-    print("total number of cut points: ", len(cut_points))
+        cut_points = construct_cut_point_abstraction([initial], pure_priority_schedule, None)
+
+        print("total number of cut points: ", len(cut_points))
 
 
 if __name__ == "__main__":
